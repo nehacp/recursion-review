ac2b18460986c0d88f73abd34d3b7e90c9fcc075
nehacp <neha.chaudhary@hotmail.com>
2017-08-21 20:25:59 -0700

    parseJSON work in progress

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -3,12 +3,13 @@ describe('parseJSON', function() {
 
   it('should match the result of calling JSON.parse', function() {
     parseableStrings.forEach(function(test) {
+      console.log('test: ', test);
       var result = parseJSON(test);
       var expected = JSON.parse(test);
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
       // Replace this line with an `expect` statement that tests
       // the behavior described by the `it` string
-      throw new Error('Test is missing.')
+      expect(equality).to.equal(true);
     });
   });
 
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,110 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+// var getNestedValue = () => {
+//   var result;
+//   json = json.slice(1, json.length - 1);
+  
+//   if (json[0] === '[') {
+//     parseArray(json);
+//   }
+//   if (json[0] === '{') {
+//     parseObj(json);
+//   }
+// };
+var parseArray = (json) => {
+  var result = [];
+  var element = '';
+  json = json.slice(1, json.length - 1);
+  
+  for (var i = 0; i < json.length; i++) {
+    if (json[i] === ' ' || json[i] === ',') {
+      i++;
+    } else if (json[i] === '[') {
+      parseArray(json);
+    } else if (json[i] === '{') {
+      parseObj(json);
+    } else if (json[i] === '"') {
+      element = getValue(json, '"', i);
+      i += element.length;
+    } else {
+      element = getValue(json, ',', i);
+      i += element.length;
+    }
+    result.push(parseJSON(element)); 
+    element = '';      
+  }
+  return result;
+};
+
+var getValue = (json, character, i) => {
+  var result = '';
+  if (character === '"') {
+    result += '"';
+    i++;
+  }
+  while (json[i] !== character) {
+    result += json[i];
+    i++;
+  }
+  if (character === '"') {
+    result += '"';
+    i++;
+  }
+  return result;
+};
+
+var getObjectValue = (json, i) => {
+  var element = json[i];
+  var count = 1;
+  i++;
+  // set a var open and close
+  //check json[0] for data type
+    // if array 
+      // open is '[' 
+      // close is ']'
+    // if object 
+      // open is '{' 
+      // close is '}'
+  while (count !== 0) {
+    element;   
+  }    
+ 
+};
+
+var parseObj = () => {
+  var result = {};
+  json = json.slice(1, json.length - 1);
+  
+  if (json[0] === '[') {
+    parseArray(json);
+  }
+  if (json[0] === '{') {
+    parseObj(json);
+  }
+};
+
 var parseJSON = function(json) {
-  // your code goes here
+  
+  if (json[0] === '[') {
+    parseArray(json);
+  }
+
+  if (json[0] === '{') {
+    parseObj(json);
+  }
+
+  if (!isNaN(Number(json))) {
+    return Number(json);
+  }
+
+  var values = ['true', true, 'false', false, 'null', null];
+  var index = values.indexOf(json);
+  if ( index !== -1) {
+    return values[index + 1];
+  } else {  
+
+    return json.slice(1, json.length - 1);
+  }
+  
 };
